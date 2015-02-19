e2d2fadde9624611d53f0d919b123e2784bf7bfb
imsosharp <imsosharp@mail.com>
2015-02-19 14:51:46 +0200

    more custom/temp builds courtesy of lapeiro.

--- Autoplay/Util/Helpers/MetaHandler.cs
+++ Autoplay/Util/Helpers/MetaHandler.cs
@@ -14,6 +14,7 @@ using AIM.Autoplay.Util.Data;
 using AIM.Autoplay.Util.Objects;
 using LeagueSharp;
 using LeagueSharp.Common;
+using LeagueSharp.Common.Data;
 using SharpDX;
 
 namespace AIM.Autoplay.Util.Helpers
@@ -28,15 +29,47 @@ namespace AIM.Autoplay.Util.Helpers
 
         public static string[] AP =
         {
-            "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Azir", "Blitzcrank",
-            "Brand", "Braum", "Cassiopeia", "Chogath", "Diana", "Elise", "Evelynn", "Ezreal", "FiddleSticks", "Fizz",
-            "Galio", "Gragas", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jax", "Karma", "Karthus", "Kassadin",
-            "Katarina", "Kayle", "Kennen", "KogMaw", "LeBlanc", "Lissandra", "Lulu", "Lux", "Malphite", "Malzahar",
+            "Ahri",  "Alistar", "Anivia", "Annie", "Azir", 
+            "Brand", "Braum", "Cassiopeia", "Chogath", "Diana", "Elise", "Evelynn", "ezreal", "FiddleSticks", "Fizz",
+            "Galio", "Gragas", "Heimerdinger", "Janna", "Karma", "Karthus", "Kassadin",
+            "Kayle",  "KogMaw", "LeBlanc", "Lissandra", "Lulu", "Lux", "Malphite", "Malzahar",
             "Maokai", "Morderkaiser", "Morgana", "Nami", "Nautilus", "Nidalee", "Nunu", "Orianna", "RekSai", "Rumble",
             "Ryze", "Shaco", "Singed", "Sona", "Soraka", "Swain", "Syndra", "Teemo", "Thresh", "TwistedFate", "veigar",
-            "VelKoz", "Viktor", "Vladimir", "Xerath", "XinZhao", "Yorick", "Ziggs", "Zilean", "Zyra"
+            "VelKoz", "Viktor", "Vladimir", "Xerath", "Ziggs", "Zilean", "Zyra"
         };
-
+		public static string[] AD =
+        {
+            "ashe", "caitlyn", "corki", "draven", "ezreal", "graves", "kogmaw",
+            "missfortune", "quinn", "sivir", "talon", "tristana", "twitch", "urgot", "varus", "vayne", "zed", "jinx",
+            "yasuo", "lucian", "shaco", "masteryi"	
+        };
+		private static readonly string[] ADT =
+        {
+            "darius",  "evelynn", "fiora", "gangplank", "gnar", "jayce", "yorick", "warwick",
+            "pantheon", "irelia", "jarvaniv", "jax", "khazix", "nocturne", "olaf", 
+            "rengar", "riven", "shyvana", "trundle", "tryndamere", "udyr", "vi", "monkeyking", "xinzhao", "aatrox",
+             
+        };
+		 private static readonly string[] APT =
+        {
+              "galio",  "malphite", "poppy", "elise",
+            "maokai", "rammus", "sejuani", "shen", "singed", "zac",
+            "nunu",  "alistar", "nautilus", "rumble", 
+        };
+		private static readonly string[] FT =
+        {
+	    "drmundo", "skarner", "braum", "nasus", "renekton", "chogath", "taric", "leesin", "garen", "volibear", "hecarim",
+	    "Blitzcrank", "amumu",
+		
+		};
+		
+	
+		private static readonly string[] EN =
+        {
+	    "Akali", "Kennen", "Katarina",
+		
+		};
+		
         private static readonly ItemId[] SRShopList =
         {
             ItemId.Zhonyas_Hourglass, ItemId.Rabadons_Deathcap,
@@ -55,18 +88,39 @@ namespace AIM.Autoplay.Util.Helpers
         private static readonly ItemId[] ARAMShopListAP =
         {
             ItemId.Zhonyas_Hourglass, ItemId.Rabadons_Deathcap,
-            ItemId.Rod_of_Ages, ItemId.Needlessly_Large_Rod, ItemId.Chalice_of_Harmony, ItemId.Rylais_Crystal_Scepter,
-            ItemId.Will_of_the_Ancients, ItemId.Locket_of_the_Iron_Solari, ItemId.Void_Staff, ItemId.Iceborn_Gauntlet,
-            ItemId.Abyssal_Scepter, ItemId.Sorcerers_Shoes
+            ItemId.Rod_of_Ages, ItemId.Rylais_Crystal_Scepter,ItemId.Athenes_Unholy_Grail,
+            ItemId.Will_of_the_Ancients, ItemId.Liandrys_Torment, ItemId.Void_Staff, ItemId.Abyssal_Scepter, ItemId.Sorcerers_Shoes
+        };
+
+	 private static readonly ItemId[] ARAMShopListEN =
+        {
+            ItemId.Zhonyas_Hourglass, ItemId.Rabadons_Deathcap,
+            ItemId.Rylais_Crystal_Scepter,
+            ItemId.Will_of_the_Ancients, ItemId.Liandrys_Torment, ItemId.Void_Staff, ItemId.Abyssal_Scepter, ItemId.Sorcerers_Shoes
+        };
+		
+		private static readonly ItemId[] ARAMShopListFT =
+        {
+            ItemId.Sunfire_Cape, ItemId.Randuins_Omen,
+            ItemId.Spirit_Visage, ItemId.Banshees_Veil, ItemId.Frozen_Heart,
+            ItemId.Locket_of_the_Iron_Solari, ItemId.Orb_of_Winter, ItemId.Thornmail, ItemId.Mercurys_Treads
         };
 
         private static readonly ItemId[] ARAMShopListAD =
         {
-            ItemId.Blade_of_the_Ruined_King, ItemId.Infinity_Edge,
-            ItemId.Phantom_Dancer, ItemId.Sanguine_Blade, ItemId.Mercurial_Scimitar, ItemId.Guardian_Angel,
-            ItemId.Banshees_Veil, ItemId.Youmuus_Ghostblade, ItemId.Berserkers_Greaves
+            ItemId.Blade_of_the_Ruined_King, ItemId.Last_Whisper,
+            ItemId.Phantom_Dancer, ItemId.Infinity_Edge, ItemId.Statikk_Shiv, ItemId.Trinity_Force, ItemId.Youmuus_Ghostblade, ItemId.Berserkers_Greaves
         };
-
+		private static readonly ItemId[] ARAMShopListADT =
+        {
+            ItemId.Maw_of_Malmortius, ItemId.Banshees_Veil, ItemId.Sunfire_Cape,
+            ItemId.Locket_of_the_Iron_Solari, ItemId.Blade_of_the_Ruined_King, ItemId.Randuins_Omen, ItemId.Spirit_Visage, ItemId.Mercurys_Treads
+        };
+		private static readonly ItemId[] ARAMShopListAPT =
+        {
+            ItemId.Abyssal_Scepter, ItemId.Banshees_Veil, ItemId.Sunfire_Cape,
+            ItemId.Locket_of_the_Iron_Solari, ItemId.Liandrys_Torment, ItemId.Randuins_Omen, ItemId.Spirit_Visage, ItemId.Mercurys_Treads
+        };
         private static readonly ItemId[] CrystalScar =
         {
             ItemId.Rod_of_Ages_Crystal_Scar, ItemId.Wooglets_Witchcap,
@@ -133,7 +187,7 @@ namespace AIM.Autoplay.Util.Helpers
 
         public static void BuyItem(ItemId item)
         {
-            if (Environment.TickCount - LastShopAttempt > Randoms.Rand.Next(0, 670))
+            if (Environment.TickCount - LastShopAttempt > Randoms.Rand.Next(500, 1000))
             {
                 Heroes.Me.BuyItem(item);
                 LastShopAttempt = Environment.TickCount;
@@ -153,11 +207,39 @@ namespace AIM.Autoplay.Util.Helpers
             }
             if (map.Type == Utility.Map.MapType.HowlingAbyss)
             {
+				if (APT.Any(apchamp => Heroes.Me.BaseSkinName.ToLower() == apchamp.ToLower()))
+                {
+					Console.WriteLine("APT");
+                    return ARAMShopListAPT;
+                }
+				if (ADT.Any(apchamp => Heroes.Me.BaseSkinName.ToLower() == apchamp.ToLower()))
+                {
+					Console.WriteLine("ADT");
+                    return ARAMShopListADT;
+                }
                 if (AP.Any(apchamp => Heroes.Me.BaseSkinName.ToLower() == apchamp.ToLower()))
                 {
+					Console.WriteLine("APD");
                     return ARAMShopListAP;
                 }
-                return ARAMShopListAD.OrderBy(item => Randoms.Rand.Next()).ToArray();
+				if (FT.Any(FTchamp => Heroes.Me.BaseSkinName.ToLower() == FTchamp.ToLower()))
+                {
+					Console.WriteLine("FT");
+                    return ARAMShopListFT;
+                }
+				if (EN.Any(ENchamp => Heroes.Me.BaseSkinName.ToLower() == ENchamp.ToLower()))
+                {
+					Console.WriteLine("EN");
+                    return ARAMShopListEN;
+                }
+
+
+				 if (AD.Any(adchamp => Heroes.Me.BaseSkinName.ToLower() == adchamp.ToLower()))
+                {
+					Console.WriteLine("ADC");
+                    return ARAMShopListAD;
+                }
+                return ARAMShopListADT;
             }
             if (map.Type == Utility.Map.MapType.CrystalScar)
             {
@@ -183,7 +265,7 @@ namespace AIM.Autoplay.Util.Helpers
             {
                 const int baseRange = 16000000; //4000^2
                 return hero.IsVisible &&
-                       ObjectHandler.Get<Obj_SpawnPoint>()
+                       ObjectManager.Get<Obj_SpawnPoint>()
                            .Any(sp => sp.Team == hero.Team && hero.Distance(sp.Position, true) < baseRange);
             }
             return false;
@@ -196,14 +278,14 @@ namespace AIM.Autoplay.Util.Helpers
 
         public static Obj_AI_Turret ClosestEnemyTurret(Vector3 point)
         {
-            var turrets = ObjectHandler.Get<Obj_AI_Turret>().FindAll(t => !t.IsAlly);
+            var turrets = ObjectManager.Get<Obj_AI_Turret>().FindAll(t => !t.IsAlly);
             return turrets.OrderBy(t => t.Distance(point)).FirstOrDefault();
         }
 
         public static Obj_AI_Minion LeadMinion()
         {
             return
-                ObjectHandler.Get<Obj_AI_Minion>()
+                ObjectManager.Get<Obj_AI_Minion>()
                     .FindAll(m => m.IsAlly)
                     .OrderBy(m => ClosestEnemyTurret(m.Position))
                     .FirstOrDefault();
@@ -212,7 +294,7 @@ namespace AIM.Autoplay.Util.Helpers
         public static Obj_AI_Minion LeadMinion(Vector3 lane)
         {
             return
-                ObjectHandler.Get<Obj_AI_Minion>()
+                ObjectManager.Get<Obj_AI_Minion>()
                     .FindAll(m => m.IsAlly)
                     .OrderBy(m => ClosestEnemyTurret(lane))
                     .FirstOrDefault();
@@ -221,14 +303,14 @@ namespace AIM.Autoplay.Util.Helpers
         public static int CountNearbyAllyMinions(Obj_AI_Base x, int distance)
         {
             return
-                ObjectHandler.Get<Obj_AI_Minion>()
+                ObjectManager.Get<Obj_AI_Minion>()
                     .Count(minion => minion.IsAlly && !minion.IsDead && minion.Distance(x) < distance);
         }
 
         public static int CountNearbyAllies(Obj_AI_Base x, int distance)
         {
             return
-                ObjectHandler.Get<Obj_AI_Hero>()
+                ObjectManager.Get<Obj_AI_Hero>()
                     .Count(
                         hero =>
                             hero.IsAlly && !hero.IsDead && !HasSmite(hero) && !hero.IsMe && hero.Distance(x) < distance);
@@ -237,7 +319,7 @@ namespace AIM.Autoplay.Util.Helpers
         public static int CountNearbyAllies(Vector3 x, int distance)
         {
             return
-                ObjectHandler.Get<Obj_AI_Hero>()
+                ObjectManager.Get<Obj_AI_Hero>()
                     .Count(
                         hero =>
                             hero.IsAlly && !hero.IsDead && !HasSmite(hero) && !hero.IsMe && hero.Distance(x) < distance);
